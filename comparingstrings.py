3836715be35465575710ea60b27c85c2c6c0ad87
saivenkat1919 <mutyalasai7@gmail.com>
2025-05-27 21:53:24 +0530

    27/05/2025

--- comparingstrings.py
+++ comparingstrings.py
@@ -108,3 +108,21 @@ for i in s:
         print(chr(ord(i)-1))
 
 
+# first word in dictionary order
+# input : He is bit slow
+# Output : bit
+s = input()
+
+first_word = s
+word = ""
+
+for i in range(len(s)):
+    char = s[i]
+    word += char
+
+    if char == " " or i == len(s) - 1:
+        if word.lower() < first_word.lower():
+            first_word = word
+        word = ""
+
+print(first_word)
